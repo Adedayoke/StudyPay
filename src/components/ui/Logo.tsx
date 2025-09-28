8927ca54558e144b97efb538928f32b9332ff172
Adedayoke <adedayoke2006@gmail.com>
2025-09-29 00:05:01 +0100

    Logo navigate to home on click

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,13 +1,16 @@
 import { StudyPayLogo } from '@/assets'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function Logo() {
   return (
-    <div className="flex items-center">
+    <Link href="/" className="block">
+      <div className="flex items-center">
         <Image src={StudyPayLogo} alt="StudyPay Logo" width={50} height={50} />
         <span className="hidden md:block ml-2 text-sm text-dark-text-secondary">Solana Campus Payments</span>
     </div>
+    </Link>
   )
 }
 
